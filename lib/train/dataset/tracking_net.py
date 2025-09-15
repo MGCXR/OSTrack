fa9d9d1685ba93374a044f844e0bc60842f2fd1c
mlc <mlc@123>
2025-09-15 11:06:36 +0800

    更新 TrackingNet 数据集的默认 set_ids，从 3 改为 5

--- lib/train/dataset/tracking_net.py
+++ lib/train/dataset/tracking_net.py
@@ -57,7 +57,7 @@ class TrackingNet(BaseVideoDataset):
         super().__init__('TrackingNet', root, image_loader)
 
         if set_ids is None:
-            set_ids = [i for i in range(3)]
+            set_ids = [i for i in range(5)]
             # set_ids = 1
 
         self.set_ids = set_ids
