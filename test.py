6b5b98b1a0bee104e535614f0ba9bb2b500f3ac9
mlc <mlc@123>
2025-09-18 15:39:21 +0800

    更新 test.py 文件，注释掉 VisEvent 数据集的相关代码，保留检查点加载和保存逻辑

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from lib.train.dataset.visevent import VisEvent
-
+import torch
 if __name__ == '__main__':
-    dataset = VisEvent()
+    # dataset = VisEvent()
 
-    print(f'First sequence: {dataset.sequence_list[0]}')
-    # print(f'Class list: {dataset._get_class_list()}')
-    print(f'Dataset size: {len(dataset)}')
-    # print(f'sequence_info: {dataset.get_sequence_info(0)}')
-    print(f'get_frames: {dataset.get_frames(0, list(range(0, 10)))}')
+    # print(f'First sequence: {dataset.sequence_list[0]}')
+    # # print(f'Class list: {dataset._get_class_list()}')
+    # print(f'Dataset size: {len(dataset)}')
+    # # print(f'sequence_info: {dataset.get_sequence_info(0)}')
+    # print(f'get_frames: {dataset.get_frames(0, list(range(0, 10)))}')
+    checkpoint = torch.load('./temp/OSTrack_ep0300.pth.tar', map_location="cpu",weights_only=False)
+    print(checkpoint.keys())
+    checkpoint['epoch'] = 1
+    torch.save(checkpoint, './temp/OSTrack_ep001.pth')
