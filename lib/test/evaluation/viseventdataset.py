ecebb76f557ed22f28e4e5ceb58637e8fd739d4a
your_username <your_email@example.com>
2025-09-25 15:22:22 +0800

    更新 VisEventDataset 类，修复 get_sequence_list 方法并添加 _construct_sequence 方法以支持序列构建

--- lib/test/evaluation/viseventdataset.py
+++ lib/test/evaluation/viseventdataset.py
@@ -16,4 +16,18 @@ class VisEventDataset(BaseDataset):
         return [entry.name for entry in os.scandir(root) if entry.is_dir()]
     
     def get_sequence_list(self):
+        return SequenceList([self._construct_sequence(set, seq_name) for set, seq_name in self.sequence_list])
+    
+    def _construct_sequence(self, set, sequence_name):
+        anno_path = os.path.join(self.base_path, set, "anno.txt")
+        ground_truth_rect = load_text(str(anno_path), delimiter=',', dtype=np.float64, backend='numpy')
         
+        frames_path = os.path.join(self.base_path, set, "frames")
+        frame_list = [frame for frame in os.listdir(frames_path) if frame.endswith(".jpg")]
+        frame_list.sort(key=lambda f: int(f[:-4]))
+        frames_list = [os.path.join(frames_path, frame) for frame in frame_list]
+        
+        return Sequence(sequence_name, frames_list, 'visevent', ground_truth_rect.reshape(-1, 4))
+    
+    def __len__(self):
+        return len(self.sequence_list)
