9fafce634a1dcaa72c3ec6e671a5e3430bc84a20
mlc <mlc@123>
2026-04-09 11:02:17 +0800

    新增多个配置文件，更新训练参数和模型设置；调整分析结果中的跟踪器列表

--- tracking/analysis_results.py
+++ tracking/analysis_results.py
@@ -46,14 +46,20 @@ trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep
                             run_ids=None, display_name='Test256_hybrid_mf'))
 trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep300_hybrid_lf', dataset_name=dataset_name,
                             run_ids=None, display_name='Test256_hybrid_lf'))
-trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep300_hybrid_hw_mf', dataset_name=dataset_name,
-                            run_ids=None, display_name='Test256_hybrid_hw_mf'))
+# trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep300_hybrid_hw_mf', dataset_name=dataset_name,
+#                             run_ids=None, display_name='Test256_hybrid_hw_mf'))
+trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep300_hybrid_hw_mf_old', dataset_name=dataset_name,
+                            run_ids=None, display_name='Test256_hybrid_hw_mf_old'))
 trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep300_hybrid_hw_mf_co', dataset_name=dataset_name,
                             run_ids=None, display_name='Test256_hybrid_hw_mf_co'))
+trackers.extend(trackerlist(name='test', parameter_name='vitb_256_mae_ce_32x4_ep300_hybrid_hw_mf_co_old', dataset_name=dataset_name,
+                            run_ids=None, display_name='Test256_hybrid_hw_mf_co_old'))
 
 dataset = get_dataset(dataset_name)
 # dataset = get_dataset('otb', 'nfs', 'uav', 'tc128ce')
 # plot_results(trackers, dataset, 'OTB2015', merge_results=True, plot_types=('success', 'norm_prec'),
 #              skip_missing_seq=False, force_evaluation=True, plot_bin_gap=0.05)
 print_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'norm_prec', 'prec'),skip_missing_seq=True)
+# plot_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'norm_prec', 'prec'), skip_missing_seq=True, force_evaluation=True, 
+            #  plot_bin_gap=0.05)
 # print_results(trackers, dataset, 'UNO', merge_results=True, plot_types=('success', 'prec'))
